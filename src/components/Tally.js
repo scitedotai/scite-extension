041b06a4a79854f9f0ae708487d9614e26abf1c1
Milo Mordaunt <milomord@gmail.com>
2019-07-22 12:42:59 -0400

    [hotfix] Tally component retries to fetch from API on failure

--- src/components/Tally.js
+++ src/components/Tally.js
@@ -19,16 +19,32 @@ class Tally extends Component {
   }
 
   componentDidMount () {
+    this.fetchReport()
+  }
+
+  fetchReport (retry = 0, maxRetries = 8) {
     const { doi } = this.props
 
+    const fetchFailed = new Error('Failed to get Tally')
+
     fetch(`https://api.scite.ai/tallies/${doi}`)
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          throw fetchFailed
+        }
+
+        return response.json()
+      })
       .then(tally => {
         if (typeof tally.total === 'number') {
           this.setState({ tally })
         }
       })
       .catch(e => {
+        if (e === fetchFailed && retry < maxRetries) {
+          return setTimeout(() => this.fetchReport(++retry, maxRetries), 1200)
+        }
+
         console.error(e)
       })
   }
