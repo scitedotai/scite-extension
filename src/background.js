51c8170bf447d193c272b51f0f8aacd8d6df2c15
Matthias Sieber <matthiasksieber@gmail.com>
2020-02-04 13:43:50 -0800

    added version listener on external message
    currently responding to messages from *.scite.ai and localhost

--- src/background.js
+++ src/background.js
@@ -1,12 +1,23 @@
 /* global chrome, browser:true */
 if (typeof chrome !== 'undefined' && chrome) {
-  browser = chrome
+  browser = chrome;
 }
 
-browser.runtime.onInstalled.addListener(function (details) {
+browser.runtime.onInstalled.addListener(function(details) {
   if (details.reason === 'install') {
     browser.tabs.create({
       url: 'https://scite.ai/extension-install'
-    })
+    });
   }
-})
+});
+
+browser.runtime.onMessageExternal.addListener((message, _sender, sendResponse) => {
+  if (message === 'version') {
+    const manifest = browser.runtime.getManifest();
+    sendResponse({
+      type: 'success',
+      version: manifest.version
+    });
+  }
+  return true;
+});
